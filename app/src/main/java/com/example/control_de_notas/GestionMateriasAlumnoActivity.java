ffa0af9bf637fe8d59434b7d04c25db3c3c1c3d5
Jafet Valencia <jafetsalo@gmail.com>
2024-04-03 03:34:37 -0500

    GestionActividades has been wired correctly, is missing a minor detail we'll handle once EditarMateria is updated. We're approaching a fully functional Materia management. Lots of changes in GestionMateriasAlumnoActivity

--- app/src/main/java/com/example/control_de_notas/GestionMateriasAlumnoActivity.java
+++ app/src/main/java/com/example/control_de_notas/GestionMateriasAlumnoActivity.java
@@ -1,10 +1,18 @@
 package com.example.control_de_notas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Vector;
 
 public class GestionMateriasAlumnoActivity extends BaseActivity {
 
@@ -13,6 +21,65 @@ public class GestionMateriasAlumnoActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materias);
 
+
+        //Aquí *
+        alumnoAEditar = buscarAlumnoNuevo();
+        //*TO DO: Pendiente asignar Alumno a editar según si viene de Crear o Editar Alumno
+
+        spinnerMateriaAlumno = (Spinner)findViewById(R.id.spinnerMateriasAlumno);
+        spinnerMateriaCatalogo = (Spinner)findViewById(R.id.spinnerMateriasCatalogo);
+
+        nombreAlumnoEncabezado = (TextView)findViewById(R.id.textViewNombreAlumno);
+        nombreAlumnoEncabezado.setText(new String("ALUMNO: " + alumnoAEditar.NombreAlumno));
+
+        recargarSpinners();
+
+
+
+    }
+
+
+    Spinner spinnerMateriaCatalogo;
+    Spinner spinnerMateriaAlumno;
+    Alumno alumnoAEditar;
+    TextView nombreAlumnoEncabezado;
+
+    public void cargarSpinnerConMaterias(@NonNull Vector<Materia> materiaVector, Spinner spinnerToLoad)
+    {
+
+        if (materiaVector.isEmpty())
+        {
+            ShowToast("No es posible cancelar materias. No hay ninguna registrada");
+            ArrayAdapter<String> emptyAdapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new String[] {});
+            spinnerToLoad.setAdapter(emptyAdapter);
+            return;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+               crearArregloMateriasVector(materiaVector));
+
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+
+       spinnerToLoad.setAdapter(adapter);
+    }
+
+    public String[] crearArregloMateriasVector(@NonNull Vector<Materia> materiaVector)
+    {
+        int numeroMaterias = materiaVector.size();
+        String[] ArregloMaterias = new String[numeroMaterias];
+        int x = 0;
+        for (Materia curso: materiaVector)
+        {
+            ArregloMaterias[x] = (x+1) + " : " + curso.NombreMateria;
+            x++;
+        }
+
+        return ArregloMaterias;
+    }
+
+    public Alumno buscarAlumnoNuevo()
+    {
+        return DatosPrograma.ListaEstudiantes.lastElement();
     }
 
     public void GoToMainActivity(View main)
@@ -21,8 +88,74 @@ public class GestionMateriasAlumnoActivity extends BaseActivity {
         //That was the best command :_)
         intentMateria.putExtra("DatosPrograma",DatosPrograma);
         startActivity(intentMateria);
+    }
+
+    public void MatricularMateriaAlumno(View materia) {
+        int indiceMateria = spinnerMateriaCatalogo.getSelectedItemPosition();
+
+        if (indiceMateria == AdapterView.INVALID_POSITION) {
+            ShowToast("No es posible MATRICULAR materias. No hay ninguna registrada");
+        } else if (isMateriaAlreadyMatriculated(indiceMateria)) {
+            ShowToast("No es posible MATRICULAR materias. Ya está matriculada");
+        } else {
+            alumnoAEditar.Materias.add(DatosPrograma.ListaMaterias.elementAt(indiceMateria));
+            ShowToast("Materia: "+ DatosPrograma.ListaMaterias.elementAt(indiceMateria).NombreMateria + " matriculada correctamente");
+        }
+
+        recargarSpinners();
+    }
+
+    private boolean isMateriaAlreadyMatriculated(int indiceMateria) {
+        for (Materia curso : alumnoAEditar.Materias) {
+            if (curso.equals(DatosPrograma.ListaMaterias.elementAt(indiceMateria))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void CancelarMateriaAlumno(View materia)
+    {
+        ShowToast("INDICE: "+ spinnerMateriaAlumno.getSelectedItemPosition());
+        if(spinnerMateriaAlumno.getSelectedItemPosition() == AdapterView.INVALID_POSITION)
+        {
+            ShowToast("No es posible CANCELAR materias. No hay ninguna registrada");
+            recargarSpinners();
+            return;
+        }
+
+        int indiceMateria = spinnerMateriaAlumno.getSelectedItemPosition();
+
+        if (indiceMateria<alumnoAEditar.Materias.size()) {
+            ShowToast("Materia: " + alumnoAEditar.Materias.get(indiceMateria).NombreMateria + " cancelada correctamente");
+            alumnoAEditar.Materias.removeElement(alumnoAEditar.Materias.get(indiceMateria));
+
+        }
+       recargarSpinners();
 
     }
 
+    public void recargarSpinners()
+    {
+
+        cargarSpinnerConMaterias(DatosPrograma.ListaMaterias, spinnerMateriaCatalogo);
+        if (DatosPrograma.ListaMaterias.isEmpty())
+        {
+            //ShowToast("No es posible agregar materias. No hay ninguna registrada");
+        }
+        else {spinnerMateriaCatalogo.setSelection(0);}
+
+
+        cargarSpinnerConMaterias(alumnoAEditar.Materias, spinnerMateriaAlumno);
+        if (alumnoAEditar.Materias.isEmpty())
+        {
+            // ShowToast("No es posible cancelar materias. No hay ninguna registrada");
+
+        }
+        else {spinnerMateriaAlumno.setSelection(0);}
+
+
+
+    }
 
 }
